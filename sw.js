1e2c3d37ee27f718a9bb124037fd7ad0ff7887ff
tyrrenh-oss <tyrrenh@gmail.com>
2026-09-25 16:31:39 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='froffies-dondads-v4';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
